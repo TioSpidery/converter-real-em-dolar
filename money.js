0945a48775fa91c3bc0a94cd9898b1fa88423457
TioSpidery <esauxd860@gmail.com>
2026-08-22 22:20:54 -0300

    atualização

---
+++ money.js
@@ -0,0 +1,14 @@
+const valorReal = document.querySelector('input[placeholder="Valor em Reais"]');
+const valorDolar = document.querySelector('input[placeholder="Valor em Dolar"]');
+const botao = document.querySelector("button");
+
+botao.addEventListener("click", function () {
+
+    const real = Number(valorReal.value);
+
+    const cotacao = 5.50;
+
+    const dolar = real / cotacao;
+
+    valorDolar.value = dolar.toFixed(2);
+});
